5e19512451a2af42b73cc9cd0c2f8577e1edf1fe
camillyMelo <93149857+camillyMelo@users.noreply.github.com>
2023-05-22 20:06:58 -0300

    Commit final

--- app/src/main/java/br/edu/ifsp/arq/minhastarefas/model/dao/TarefaDaoSingleton.java
+++ app/src/main/java/br/edu/ifsp/arq/minhastarefas/model/dao/TarefaDaoSingleton.java
@@ -1,5 +1,13 @@
 package br.edu.ifsp.arq.minhastarefas.model.dao;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -7,18 +15,18 @@ import java.util.List;
 
 import br.edu.ifsp.arq.minhastarefas.model.entities.Tag;
 import br.edu.ifsp.arq.minhastarefas.model.entities.Tarefa;
+import br.edu.ifsp.arq.minhastarefas.utils.Constantes;
 
 public class TarefaDaoSingleton implements ITarefaDao {
     private static TarefaDaoSingleton instance = null;
     private List<Tarefa> tarefaList;
-
+    private Context context;
     private TarefaDaoSingleton() {
         tarefaList = new ArrayList<Tarefa>();
         tarefaList.add(new Tarefa("Prova Redes","Materia: Camada de redes", "23/5/2023", 1));
         tarefaList.add(new Tarefa("Projeto DMO","Primeira fase feita","22/5/2023", 1));
         tarefaList.add(new Tarefa("Stop Motion IHC2", "Adicionar efeitos sonoros","24/5/2023", 2));
         tarefaList.add(new Tarefa("Site Web","Alterar css", "26/5/2023", 3));
-
 
     }
 
@@ -33,6 +41,8 @@ public class TarefaDaoSingleton implements ITarefaDao {
         if(tarefa != null){
             tarefaList.add(tarefa);
             Collections.sort(tarefaList, comparatorTarefa);
+            adicionarTarefaBanco();
+            pegarTarefaBanco();
         }
     }
 
@@ -49,6 +59,8 @@ public class TarefaDaoSingleton implements ITarefaDao {
             inDataset.getTags().clear();
             inDataset.getTags().addAll(tarefa.getTags());
             Collections.sort(tarefaList, comparatorTarefa);
+            adicionarTarefaBanco();
+            pegarTarefaBanco();
             return true;
         }
         return false;
@@ -56,7 +68,10 @@ public class TarefaDaoSingleton implements ITarefaDao {
 
     @Override
     public boolean delete(Tarefa tarefa) {
-        return tarefaList.remove(tarefa);
+        tarefaList.remove(tarefa);
+        adicionarTarefaBanco();
+        pegarTarefaBanco();
+        return true;
     }
 
     @Override
@@ -84,9 +99,64 @@ public class TarefaDaoSingleton implements ITarefaDao {
         return selection;
     }
 
-
+    public void setContext(Context context){
+        this.context = context;
+    }
     @Override
-    public List<Tarefa> findAll() {
+    public List<Tarefa> findAll() { pegarTarefaBanco();
         return tarefaList;
+    }
+
+    private void adicionarTarefaBanco(){
+        SharedPreferences preferences;
+        SharedPreferences.Editor editor;
+
+        JSONObject jsonObject;
+        JSONArray jsonArray;
+
+        jsonArray = new JSONArray();
+        for(Tarefa t : tarefaList){
+            jsonObject = new JSONObject();
+            try{
+                jsonObject.put(Constantes.ATTR_TITLE, t.getNomeTarefa());
+                jsonObject.put(Constantes.ATTR_DESCRIPTION, t.getDescricao());
+                jsonObject.put(Constantes.ATTR_DATE, t.getData());
+                jsonObject.put(Constantes.ATTR_PRIORITY, t.getPrioridade());
+                jsonArray.put(jsonObject);
+            }catch (JSONException e){
+                Log.e("Erro", e.getMessage());
+            }
+        }
+        preferences = context.getSharedPreferences(Constantes.DATABASE_FILE_NAME, Context.MODE_PRIVATE);
+        editor = preferences.edit();
+        editor.putString(Constantes.TABLE_NAME, jsonArray.toString());
+        editor.commit();
+
+    }
+    private void pegarTarefaBanco(){
+        SharedPreferences preferences;
+        String json;
+        Tarefa tarefa;
+        JSONObject jsonObject;
+        JSONArray jsonArray;
+
+        preferences = context.getSharedPreferences(Constantes.DATABASE_FILE_NAME, Context.MODE_PRIVATE);
+        json = preferences.getString(Constantes.TABLE_NAME, "");
+
+        if(!json.isEmpty()){
+            tarefaList.clear();
+            try{
+                jsonArray = new JSONArray(json);
+                for(int i=0; i < jsonArray.length(); i++){
+                    jsonObject = jsonArray.getJSONObject(i);
+                    tarefa = new Tarefa(jsonObject.getString(Constantes.ATTR_TITLE), jsonObject.getString(Constantes.ATTR_DESCRIPTION), jsonObject.getString(Constantes.ATTR_DATE), jsonObject.getInt(Constantes.ATTR_PRIORITY));
+                    tarefaList.add(tarefa);
+                }
+            }catch (JSONException e){
+                Log.e("TarefaDAOJson", e.getMessage());
+            }
+        }else{
+            Log.v("TarefaDAOJson", "Sem dados");
+        }
     }
 }
