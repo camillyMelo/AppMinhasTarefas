6d836d2ca60ea52b4ab5f10d77f407dca4861fe5
camillyMelo <93149857+camillyMelo@users.noreply.github.com>
2023-05-22 20:08:25 -0300

    Commit final

--- app/src/main/java/br/edu/ifsp/arq/minhastarefas/presenter/MainPresenter.java
+++ app/src/main/java/br/edu/ifsp/arq/minhastarefas/presenter/MainPresenter.java
@@ -1,5 +1,6 @@
 package br.edu.ifsp.arq.minhastarefas.presenter;
 
+import android.content.Context;
 import android.content.Intent;
 
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -17,10 +18,12 @@ public class MainPresenter implements MainMVP.Presenter {
 
     private MainMVP.View view;
     private ITarefaDao dao;
-
+    private Context context;
     public MainPresenter(MainMVP.View view) {
         this.view = view;
         dao = TarefaDaoSingleton.getInstance();
+        context = view.getContext();
+        dao.setContext(context);
     }
 
     @Override
